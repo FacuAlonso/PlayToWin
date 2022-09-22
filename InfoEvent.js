32b6b09382f2889244ba1094a16c1991d6453c4a
Nachoo04 <112171727+Nachoo04@users.noreply.github.com>
2022-09-22 11:52:51 -0300

    jj

---
+++ InfoEvent.js
@@ -0,0 +1,8 @@
+function abrir() {
+    var element = document.getElementById("popup-caja");
+    element.classList.add("visibilidad");
+}
+function cerrar() {
+    var element = document.getElementById("popup-caja");
+    element.classList.remove("visibilidad");
+}

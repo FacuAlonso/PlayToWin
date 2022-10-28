552d4c397cdb4f58dd19eed65cd36fa7ec9730c6
Nachoo04 <112171727+Nachoo04@users.noreply.github.com>
2022-10-28 09:47:02 -0300

    k

--- header.php
+++ header.php
@@ -5,13 +5,15 @@ function genHeader($titulo){
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link href="home.css" rel="stylesheet">
+        <link href="login.css" rel="stylesheet">
+        <script language="javascript" type="text/javascript" src="InfoEvent.js"></script>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/x-icon" href="assets\JoystickFAVICON64.png">
         <title>$titulo</title>
     </head>
     HEAD;
-}
+
 echo($header);
+}
 ?>

23d4928ed69ab40f287090037892ffca9f52925c
FacuAlonso <112171600+FacuAlonso@users.noreply.github.com>
2022-10-28 08:26:15 -0300

    ouui

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+function genHeader($titulo){
+    $header = <<<HEAD
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="home.css" rel="stylesheet">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="assets\JoystickFAVICON64.png">
+        <title>$titulo</title>
+    </head>
+    HEAD;
+}
+echo($header);
+?>
--- home.php
+++ home.php
@@ -1,17 +1,11 @@
-<?php include 'database.php'; 
-$eventosActivos = listaEventos();
+<?php 
+    include 'database.php'; 
+    include 'header.php';
+    $eventosActivos = listaEventos();
+    $head = genHeader("Home | Play to Win");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link href="home.css" rel="stylesheet">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="assets\JoystickFAVICON64.png">
-    <title>Home | Play to Win</title>
-</head>
+
 <body>
     <div id="encabezado">
         <div id="marca">

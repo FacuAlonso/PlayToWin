93a0a4392bdbde73fa79335991c8f807b8109fde
Nachoo04 <112171727+Nachoo04@users.noreply.github.com>
2022-11-04 08:42:03 -0300

    Revert "Merge branch 'php-env' of https://github.com/FacuAlonso/PlayToWin into php-env"
    This reverts commit 8de4e8f3559f47936c4417e20bc2dc8c36819e89, reversing
    changes made to 552d4c397cdb4f58dd19eed65cd36fa7ec9730c6.

--- header.php
+++ header.php
@@ -1,17 +1,19 @@
 <?php
-function genHeader($titulo,$cssFile){
+function genHeader($titulo){
     $header = <<<HEAD
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link href=$cssFile rel="stylesheet">
+        <link href="login.css" rel="stylesheet">
+        <script language="javascript" type="text/javascript" src="InfoEvent.js"></script>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/x-icon" href="assets\JoystickFAVICON64.png">
         <title>$titulo</title>
     </head>
     HEAD;
-    echo($header);
+
+echo($header);
 }
 ?>

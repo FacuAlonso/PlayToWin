5d3bf4f2cc95e24ce67b1cd8495f2c1906883ba7
Nachoo04 <112171727+Nachoo04@users.noreply.github.com>
2022-12-01 13:59:58 -0300

    pepe

--- signup.php
+++ signup.php
@@ -1,30 +1,36 @@
 <?php
 include 'auxPHP/header.php';
-$head = genHeader("Login | Play to Win",'css/signup.css',"js/InfoEvent.js");
 
-$body = <<<BODY
-        <body>
-            <a id="encabezado"><img id="logo" src="assets\playtowinICONO.png"></a>
-            <form  onsubmit="return ValidarSingUp('box-login','nom01', 'ape01', 'clv01')" action='auxPHP/altausuario.php' method="post" id="box-login">
-                <p class="titulo">Crea tu cuenta</p>
-                <label for="nom01" class="datos-titulo">Email:</label>
-                <input type="email" class="datos-caja" name="usuario" id="nom01" placeholder="usuario@email.com" required/>
-                <label for="ape01" class="datos-titulo">Contraseña:</label>
-                <input type="password" class="datos-caja" name="pass" id="ape01" placeholder="8 caracteres, combinación de números y letras" required
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Debe contenter 8 caracteres al menos, y debe ser combinación de números, minúsculas y mayúsculas."/>
-                <label for="clv01" class="datos-titulo">Confirma tu contraseña:</label>
-                <input type="password" class="datos-caja" name="clave" id="clv01" placeholder="Confirma la contraseña ingresada"/>
-                <input type="submit" class="datos-registro" value="REGISTRARSE" id="boton-submit">
-                <a href="login.php" id="yatienecuenta">¿Ya tienes una cuenta? <b>INICIA SESIÓN</b></a>
-            </form>
-        </body>
-    BODY;
-$sitio= <<<Sitio
-<!DOCTYPE html>
-    <html lang="es">
-    $head
-    $body
-    </html>
-Sitio;
-echo($sitio);
+function GenerarPag(){
+    $head = genHeader("Login | Play to Win",'css/signup.css',"js/InfoEvent.js");
+    $body = <<<BODY
+            <body>
+                <a id="encabezado"><img id="logo" src="assets\playtowinICONO.png"></a>
+                <form  onsubmit="return ValidarSingUp('box-login','nom01', 'ape01', 'clv01')" action='auxPHP/altausuario.php' method="post" id="box-login">
+                    <p class="titulo">Crea tu cuenta</p>
+                    <label for="nom01" class="datos-titulo">Email:</label>
+                    <input type="email" class="datos-caja" name="usuario" id="nom01" placeholder="usuario@email.com" required/>
+                    <label for="ape01" class="datos-titulo">Contraseña:</label>
+                    <input type="password" class="datos-caja" name="pass" id="ape01" placeholder="8 caracteres, combinación de números y letras" required
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Debe contenter 8 caracteres al menos, y debe ser combinación de números, minúsculas y mayúsculas."/>
+                    <label for="clv01" class="datos-titulo">Confirma tu contraseña:</label>
+                    <input type="password" class="datos-caja" name="clave" id="clv01" placeholder="Confirma la contraseña ingresada"/>
+                    <input type="submit" class="datos-registro" value="REGISTRARSE" id="boton-submit">
+                    <a href="login.php" id="yatienecuenta">¿Ya tienes una cuenta? <b>INICIA SESIÓN</b></a>
+                </form>
+            </body>
+        BODY;
+    $sitio= <<<Sitio
+    <!DOCTYPE html>
+        <html lang="es">
+        $head
+        $body
+        </html>
+    Sitio;
+    echo($sitio);
+}
+function main(){
+    GenerarPag();
+}
+main();
 ?>

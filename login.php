9b3e789f41dfb9adf69fd9a8e7cfa1d4d89c9ab7
Nachoo04 <112171727+Nachoo04@users.noreply.github.com>
2022-12-01 17:07:20 -0300

    login

--- login.php
+++ login.php
@@ -1,38 +1,44 @@
 <?php 
-    include 'auxPHP/header.php';
-    $head = genHeader("Login | Play to Win",'css/login.css',"js/InfoEvent.js");
+        include 'auxPHP/header.php';
+    function GenerarPag(){
+        $head = genHeader("Login | Play to Win",'css/login.css',"js/InfoEvent.js");
 
-    if (isset($_GET['err'])){
-        if ($_GET['err']=='denegado'){
-            $cartelError = "<p class='bad'>EL USUARIO O CONTRASEÑA SON INCORRECTOS</p>";
-        }elseif($_GET['err']=='yaexiste'){
-            $cartelError = "<p class='bad'>YA EXISTE UN USUARIO CON ESE EMAIL. INICIA SESIÓN.</p>";
+        if (isset($_GET['err'])){
+            if ($_GET['err']=='denegado'){
+                $cartelError = "<p class='bad'>EL USUARIO O CONTRASEÑA SON INCORRECTOS</p>";
+            }elseif($_GET['err']=='yaexiste'){
+                $cartelError = "<p class='bad'>YA EXISTE UN USUARIO CON ESE EMAIL. INICIA SESIÓN.</p>";
+            }
+        }else{$cartelError = "";
         }
-     }else{$cartelError = "";
-    }
-    
+        
 
-    $body = <<<BODY
-        <body>
-            <a id="encabezado"><img id="logo" src="assets\playtowinICONO.png"></a>
-            <form action='auxPHP/validarlogin.php' method="post" onsubmit="return validardatos('box-login','nom01', 'ape01')" name="Datos" id="box-login">
-                <p class="titulo">Accede a tu cuenta</p>
-                <label for="nom01" class="datos-titulo">Email:</label>
-                <input required type="email" class="datos-caja" name="usuario" id="nom01" placeholder="usuario@email.com"/>
-                <label for="ape01" class="datos-titulo">Contraseña:</label>
-                <input required type="password" class="datos-caja" name="pass" id="ape01" placeholder="Ingresa tu contraseña aquí"/>
-                $cartelError
-                <input type="submit" class="datos-registro" value="INICIAR SESIÓN" id="boton-submit">
-                <a href="signup.php" id="yatienecuenta">¿No tienes una cuenta aún? <b>CREA TU CUENTA AQUÍ</b></a>
-            </form>
-        </body>
-    BODY;
-    $paginalog= <<<SITIO
-    <!DOCTYPE html>
-    <html lang="es">
-    $head
-    $body
-    </html>
-    SITIO;
-    echo($paginalog);
+        $body = <<<BODY
+            <body>
+                <a id="encabezado"><img id="logo" src="assets\playtowinICONO.png"></a>
+                <form action='auxPHP/validarlogin.php' method="post" onsubmit="return validardatos('box-login','nom01', 'ape01')" name="Datos" id="box-login">
+                    <p class="titulo">Accede a tu cuenta</p>
+                    <label for="nom01" class="datos-titulo">Email:</label>
+                    <input required type="email" class="datos-caja" name="usuario" id="nom01" placeholder="usuario@email.com"/>
+                    <label for="ape01" class="datos-titulo">Contraseña:</label>
+                    <input required type="password" class="datos-caja" name="pass" id="ape01" placeholder="Ingresa tu contraseña aquí"/>
+                    $cartelError
+                    <input type="submit" class="datos-registro" value="INICIAR SESIÓN" id="boton-submit">
+                    <a href="signup.php" id="yatienecuenta">¿No tienes una cuenta aún? <b>CREA TU CUENTA AQUÍ</b></a>
+                </form>
+            </body>
+        BODY;
+        $paginalog= <<<SITIO
+        <!DOCTYPE html>
+        <html lang="es">
+        $head
+        $body
+        </html>
+        SITIO;
+        echo($paginalog);
+    }
+function main(){
+    GenerarPag();
+}
+main();
 ?>

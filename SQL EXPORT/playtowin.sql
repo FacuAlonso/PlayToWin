d2a564451df022a92b67a412bcfb04d72105e233
dimpe66 <112214241+dimpe66@users.noreply.github.com>
2023-02-15 22:04:53 -0300

    Update playtowin.sql

--- SQL EXPORT/playtowin.sql
+++ SQL EXPORT/playtowin.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-12-2022 a las 13:28:19
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 8.1.10
+-- Host: 127.0.0.1
+-- Generation Time: Feb 16, 2023 at 02:03 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `playtowin`
+-- Database: `playtowin`
 --
 CREATE DATABASE IF NOT EXISTS `playtowin` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `playtowin`;
@@ -26,50 +26,50 @@ USE `playtowin`;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `eventos`
+-- Table structure for table `eventos`
 --
 
 CREATE TABLE `eventos` (
   `id` int(11) NOT NULL,
-  `nomEvento` varchar(300) COLLATE utf8_bin NOT NULL,
+  `nomEvento` varchar(300) NOT NULL,
   `preset` int(11) NOT NULL COMMENT 'depende de id en presets',
-  `descEvento` text COLLATE utf8_bin NOT NULL,
-  `reglasEvento` text COLLATE utf8_bin NOT NULL,
+  `descEvento` text NOT NULL,
+  `reglasEvento` text NOT NULL,
   `fechaFinal` datetime NOT NULL DEFAULT current_timestamp(),
-  `estado` varchar(30) COLLATE utf8_bin NOT NULL DEFAULT 'activo'
+  `estado` varchar(30) NOT NULL DEFAULT 'activo'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Volcado de datos para la tabla `eventos`
+-- Dumping data for table `eventos`
 --
 
 INSERT INTO `eventos` (`id`, `nomEvento`, `preset`, `descEvento`, `reglasEvento`, `fechaFinal`, `estado`) VALUES
-(1, '¡Logra la mayor cantidad de elixir Quemado!\r\n\r\n', 3, 'Juega una partida normal de Clash Royale intentando quemar la mayor cantidad de elixir.\r\n\r\nCuando estés conforme con tu puntaje obtenido, haz click en PARTICIPAR y completa los datos solicitados.', 'No cuentan partidas personalizadas. La partida debe haber sido jugada durante el período del evento.', '2022-12-03 00:00:00', 'activo'),
-(2, '¡Haz la mayor cantidad de kills!', 2, 'Descripción del evento de CoD Mobile, donde los jugadores deben hacer kills. Descripción del evento de CoD Mobile, donde los jugadores deben hacer kills. Descripción del evento de CoD Mobile, donde los jugadores deben hacer kills.', 'No cuentan partidas personalizadas. La partida debe haber sido jugada durante el período del evento.', '2022-12-12 15:49:00', 'activo'),
-(3, '¡Camina la mayor distancia en el mapa!\r\n\r\n', 5, 'Descripción del evento de Fortnite, donde los jugadores deben indicar el puntaje en metros recorridos.', 'No cuentan partidas personalizadas. La partida debe haber sido jugada durante el período del evento.', '2022-12-08 11:00:00', 'activo'),
-(18, '¡Haz la mayor cantidad de goles!', 44, '¡Juega una partida en línea de FIFA 22, e indica cuántos goles has hecho! Cuando estés conforme con tu puntaje obtenido, haz click en PARTICIPAR y completa los datos solicitados.', '- No valen partidas personalizadas ni locales (no online)', '2022-12-04 17:21:00', 'activo'),
-(20, '¡Logra hacer la mayor cantidad de puntos!', 46, 'Juega una partida de Pac-Man, intentando conseguir el mayor puntaje. Cuando estés conforme con tu puntaje obtenido, haz click en PARTICIPAR y completa los datos solicitados.', '- No valen partidas modificadas/personalizadas', '2022-12-06 18:01:00', 'activo'),
-(21, 'Gana una partida en el menor tiempo', 47, 'Demuestra tu habilidad en CS:GO e intenta ganar una partida en el menor tiempo posible. Ingresa el tiempo en cantidad de segundos. Cuando estés conforme con tu puntaje obtenido, haz click en PARTICIPAR y completa los datos solicitados.', '- Deben ser partidas de 30 rondas - Se puede usar cualquier arma - Modo solitario - Partidas oficiales', '2022-12-14 18:04:00', 'activo'),
+(1, '¡Logra la mayor cantidad de elixir Quemado!\r\n\r\n', 3, 'Juega una partida normal de Clash Royale intentando quemar la mayor cantidad de elixir.\r\n\r\nCuando estés conforme con tu puntaje obtenido, haz click en PARTICIPAR y completa los datos solicitados.', 'No cuentan partidas personalizadas. La partida debe haber sido jugada durante el período del evento.', '2022-12-03 00:00:00', 'finalizado'),
+(2, '¡Haz la mayor cantidad de kills!', 2, 'Descripción del evento de CoD Mobile, donde los jugadores deben hacer kills. Descripción del evento de CoD Mobile, donde los jugadores deben hacer kills. Descripción del evento de CoD Mobile, donde los jugadores deben hacer kills.', 'No cuentan partidas personalizadas. La partida debe haber sido jugada durante el período del evento.', '2022-12-12 15:49:00', 'finalizado'),
+(3, '¡Camina la mayor distancia en el mapa!\r\n\r\n', 5, 'Descripción del evento de Fortnite, donde los jugadores deben indicar el puntaje en metros recorridos.', 'No cuentan partidas personalizadas. La partida debe haber sido jugada durante el período del evento.', '2022-12-08 11:00:00', 'finalizado'),
+(18, '¡Haz la mayor cantidad de goles!', 44, '¡Juega una partida en línea de FIFA 22, e indica cuántos goles has hecho! Cuando estés conforme con tu puntaje obtenido, haz click en PARTICIPAR y completa los datos solicitados.', '- No valen partidas personalizadas ni locales (no online)', '2022-12-04 17:21:00', 'finalizado'),
+(20, '¡Logra hacer la mayor cantidad de puntos!', 46, 'Juega una partida de Pac-Man, intentando conseguir el mayor puntaje. Cuando estés conforme con tu puntaje obtenido, haz click en PARTICIPAR y completa los datos solicitados.', '- No valen partidas modificadas/personalizadas', '2022-12-06 18:01:00', 'finalizado'),
+(21, 'Gana una partida en el menor tiempo', 47, 'Demuestra tu habilidad en CS:GO e intenta ganar una partida en el menor tiempo posible. Ingresa el tiempo en cantidad de segundos. Cuando estés conforme con tu puntaje obtenido, haz click en PARTICIPAR y completa los datos solicitados.', '- Deben ser partidas de 30 rondas - Se puede usar cualquier arma - Modo solitario - Partidas oficiales', '2022-12-14 18:04:00', 'finalizado'),
 (22, 'Realiza la mayor cantidad de bajas con cuchillo', 1, 'En partidas normales, realiza la mayor cantidad de bajas con cuchillo que puedas. Cuando estés conforme con tu puntaje obtenido, haz click en PARTICIPAR y completa los datos solicitados.', 'Sólo puedes usar el cuchillo como arma - Puedes jugar en equipo', '2022-12-01 18:15:00', 'finalizado');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `participaciones`
+-- Table structure for table `participaciones`
 --
 
 CREATE TABLE `participaciones` (
   `id` int(11) NOT NULL,
   `evento` int(11) NOT NULL COMMENT 'FK',
   `usuario` int(11) NOT NULL COMMENT 'FK',
-  `nickJugador` varchar(30) COLLATE utf8_bin NOT NULL,
+  `nickJugador` varchar(30) NOT NULL,
   `puntaje` int(11) NOT NULL,
   `fechaParticipa` timestamp NOT NULL DEFAULT current_timestamp(),
   `descalificado` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Si es 1, está descalificado.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Volcado de datos para la tabla `participaciones`
+-- Dumping data for table `participaciones`
 --
 
 INSERT INTO `participaciones` (`id`, `evento`, `usuario`, `nickJugador`, `puntaje`, `fechaParticipa`, `descalificado`) VALUES
@@ -82,17 +82,17 @@ INSERT INTO `participaciones` (`id`, `evento`, `usuario`, `nickJugador`, `puntaj
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `presets`
+-- Table structure for table `presets`
 --
 
 CREATE TABLE `presets` (
   `id` int(11) NOT NULL,
-  `nomJuego` varchar(30) COLLATE utf8_bin NOT NULL,
-  `portada` varchar(255) COLLATE utf8_bin NOT NULL
+  `nomJuego` varchar(30) NOT NULL,
+  `portada` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Volcado de datos para la tabla `presets`
+-- Dumping data for table `presets`
 --
 
 INSERT INTO `presets` (`id`, `nomJuego`, `portada`) VALUES
@@ -108,18 +108,18 @@ INSERT INTO `presets` (`id`, `nomJuego`, `portada`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Table structure for table `usuarios`
 --
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `email` varchar(30) COLLATE utf8_bin NOT NULL,
-  `pass` varchar(30) COLLATE utf8_bin NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `pass` varchar(30) NOT NULL,
   `isAdmin` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Dumping data for table `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `email`, `pass`, `isAdmin`) VALUES
@@ -132,18 +132,18 @@ INSERT INTO `usuarios` (`id`, `email`, `pass`, `isAdmin`) VALUES
 (11, 'user6@mail.com', 'Pass1234', 0);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `eventos`
+-- Indexes for table `eventos`
 --
 ALTER TABLE `eventos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `conectarPresets` (`preset`);
 
 --
--- Indices de la tabla `participaciones`
+-- Indexes for table `participaciones`
 --
 ALTER TABLE `participaciones`
   ADD PRIMARY KEY (`id`),
@@ -151,59 +151,59 @@ ALTER TABLE `participaciones`
   ADD KEY `conectarEventos` (`evento`);
 
 --
--- Indices de la tabla `presets`
+-- Indexes for table `presets`
 --
 ALTER TABLE `presets`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nomJuego` (`nomJuego`);
 
 --
--- Indices de la tabla `usuarios`
+-- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `eventos`
+-- AUTO_INCREMENT for table `eventos`
 --
 ALTER TABLE `eventos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
--- AUTO_INCREMENT de la tabla `participaciones`
+-- AUTO_INCREMENT for table `participaciones`
 --
 ALTER TABLE `participaciones`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
--- AUTO_INCREMENT de la tabla `presets`
+-- AUTO_INCREMENT for table `presets`
 --
 ALTER TABLE `presets`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `eventos`
+-- Constraints for table `eventos`
 --
 ALTER TABLE `eventos`
   ADD CONSTRAINT `conectarPresets` FOREIGN KEY (`preset`) REFERENCES `presets` (`id`);
 
 --
--- Filtros para la tabla `participaciones`
+-- Constraints for table `participaciones`
 --
 ALTER TABLE `participaciones`
   ADD CONSTRAINT `conectarEventos` FOREIGN KEY (`evento`) REFERENCES `eventos` (`id`) ON DELETE CASCADE,
